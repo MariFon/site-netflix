ef74e5ce30612a0430370cd3f1d21e3d8e6bbc69
MariFon <87157578+MariFon@users.noreply.github.com>
2021-10-01 22:42:27 -0300

    site tipo-netflix
    mariflix

---
+++ app.js
@@ -0,0 +1,13 @@
+function CarregarFilmes() {
+//array = variavel com subdivisoes(indices)
+    let filmes = ["imagens/chegada.png", "imagens/escola.png", "imagens/ontem.png", "imagens/ww.png"]
+
+    // console.log("Carregar Filmes")
+    // document.querySelector(".lista-filmes").innerHTML = "<img src=" + filmes[0] + ">"
+    // document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes[1] + ">"
+    // document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes[2] + ">"
+    // document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes[3] + ">"
+
+    //laço de repetição
+    for(let controle = 0 ; controle < 6 ; controle + 1){ document.querySelector(".lista-filmes").innerHTML +="<img src" + filmes[controle] + ">"}
+}
