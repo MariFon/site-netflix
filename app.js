c54ea909a11dd019440404833159e250573e315a
MariFon <87157578+MariFon@users.noreply.github.com>
2021-10-05 20:44:51 -0300

    codigo correto
    corrigido erro do loop

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 function CarregarFilmes() {
-//array = variavel com subdivisoes(indices)
+
     let filmes = ["imagens/chegada.png", "imagens/escola.png", "imagens/ontem.png", "imagens/ww.png"]
 
-    // console.log("Carregar Filmes")
-    // document.querySelector(".lista-filmes").innerHTML = "<img src=" + filmes[0] + ">"
-    // document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes[1] + ">"
-    // document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes[2] + ">"
-    // document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes[3] + ">"
-
-    //laço de repetição
-    for(let controle = 0 ; controle < 6 ; controle = controle + 1){ document.querySelector(".lista-filmes").innerHTML +="<img src" + filmes[controle] + ">"}
+    for (let controle = 0; controle < 4; controle = controle + 1) { 
+        document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes[controle] + ">" 
+    }
 }
