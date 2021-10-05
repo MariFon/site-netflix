249c426c0ef730a76ac7c687a5ae8edefb640333
MariFon <87157578+MariFon@users.noreply.github.com>
2021-10-05 19:59:46 -0300

    codigo arrumado

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ function CarregarFilmes() {
     // document.querySelector(".lista-filmes").innerHTML += "<img src=" + filmes[3] + ">"
 
     //laço de repetição
-    for(let controle = 0 ; controle < 6 ; controle + 1){ document.querySelector(".lista-filmes").innerHTML +="<img src" + filmes[controle] + ">"}
+    for(let controle = 0 ; controle < 6 ; controle = controle + 1){ document.querySelector(".lista-filmes").innerHTML +="<img src" + filmes[controle] + ">"}
 }
